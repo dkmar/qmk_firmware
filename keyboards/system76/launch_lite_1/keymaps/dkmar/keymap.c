f5d19e527b2625adfd32b530153ed2d45fec0e57
dkmar <dkmar@protonmail.com>
2025-01-17 09:42:39 -0800

    switching from hyper to ctrl alt gui

--- keyboards/system76/launch_lite_1/keymaps/dkmar/keymap.c
+++ keyboards/system76/launch_lite_1/keymaps/dkmar/keymap.c
@@ -17,16 +17,16 @@ ________________________________________________________________________________
   |     SHIFT      |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |   <    |    >   |   ?    |   SHIFT    |   UP   |  END   |
   |________________|________|________|________|________|________|________|________|________|________|________|____________|________|________|
   |            |        |       |        |                 |                 |        |        |             |   |        |        |        |
-  |    CTRL    |  FN    | LALT  | LGUI   |    SPACE        |   BACKSPACE     |  ESC   |  RALT  |     FN      |   |  LEFT  |  DOWN  | RIGHT  |
+  |    LGUI    |  CTRL  | LALT  | LGUI   |    SPACE        |   BACKSPACE     |  ESC   |  RALT  |     FN      |   |  LEFT  |  DOWN  | RIGHT  |
   |____________|________|_______|________|_________________|_________________|________|________|_____________|   |________|________|________|
 */
 
   [0] = LAYOUT(
     KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_PGUP,
-          HYPR_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,       KC_PGDN,
-          SC_LSPO,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    SC_RSPC,    KC_UP, KC_END,
-          KC_LGUI, KC_LCTL,   KC_LALT, KC_LGUI,     KC_SPC,           MO(1),    KC_RGUI,    KC_RALT,   KC_RCTL,         KC_LEFT, KC_DOWN, KC_RGHT
+          LCAG_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,       KC_PGDN,
+          KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP, KC_END,
+          KC_LGUI, KC_LCTL,   KC_LALT, KC_LGUI,     KC_SPC,           LT(1, KC_SPC),    KC_RGUI,    KC_RALT,   KC_RCTL,         KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
     /* Layer 1, function layer
@@ -52,7 +52,7 @@ ________________________________________________________________________________
 */
 
   [1] = LAYOUT(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,      KC_MPLY,
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,      QK_BOOT,
         KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_HOME, KC_PGDN, KC_TRNS, RGB_VAD, RGB_VAI, RGB_TOG, KC_VOLU,
           KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,    KC_TRNS,     KC_VOLD,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_GRAVE,   KC_TRNS,  KC_PGUP, KC_MUTE,
