17b534702345ad672d183fcb389515be8424c5fb
dkmar <dkmar@protonmail.com>
2025-05-23 13:30:41 -0700

    added easier backtick key in fn layer. trying shift-shift for ghostty quick-terminal keybind

--- keyboards/system76/launch_lite_1/keymaps/dkmar/keymap.c
+++ keyboards/system76/launch_lite_1/keymaps/dkmar/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-// tap dance 
+// tap dance
 enum { TD_LSFT };
 typedef enum {
     TD_NONE,
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_PGUP,
           LCAG_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,       KC_PGDN,
           TD(TD_LSFT),     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP, KC_END,
-          OSM(MOD_LGUI), KC_LCTL,   KC_LALT, KC_LGUI,     KC_SPC,           LT(1, KC_SPC),    KC_RGUI,    KC_RALT,   KC_RCTL,         KC_LEFT, KC_DOWN, KC_RGHT
+          OSM(MOD_LGUI), KC_LCTL,   KC_LALT, KC_LGUI,     KC_SPC,           LT(2, KC_SPC),    OSM(MOD_RGUI),    KC_RALT,   KC_RCTL,         KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
     /* Layer 1, function layer
@@ -65,20 +65,21 @@ ________________________________________________________________________________
 */
 
   [1] = LAYOUT(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,      QK_BOOT,
-        KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_HOME, KC_PGDN, KC_TRNS, RGB_VAD, RGB_VAI, RGB_TOG, KC_VOLU,
-          KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,    KC_TRNS,     KC_VOLD,
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_GRAVE,   KC_TRNS,  KC_PGUP, KC_MUTE,
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_ENT,           KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS,       KC_HOME,  KC_PGDN, KC_END
-  ),
-
-  [2] = LAYOUT(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,     KC_TRNS,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS,
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,          KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,  KC_TRNS, KC_TRNS
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,          OSL(2),      KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,  KC_TRNS, KC_TRNS
   ),
+
+  [2] = LAYOUT(
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,      QK_BOOT,
+        KC_PSCR, S(KC_1),    S(KC_2),    S(KC_3),    S(KC_4),    S(KC_5),    S(KC_6),    S(KC_7),    S(KC_8),    S(KC_9),    S(KC_0),    S(KC_MINS), S(KC_EQL), RGB_TOG, KC_VOLU,
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_DOLLAR, KC_LEFT_PAREN, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_GRAVE,    KC_TRNS,     KC_VOLD,
+              SC_LSPO, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_DOLLAR,  KC_PGUP, KC_MUTE,
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,           KC_BSPC,       KC_TRNS, KC_TRNS, TG(1),       KC_HOME,  KC_PGDN, KC_END
+  ),
+
 
   [3] = LAYOUT(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
@@ -97,17 +98,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LCAG_T(KC_ESC):
-        case LT(1, KC_SPC):
+        case LT(2, KC_SPC):
+        case OSL(2):
             return 0;
         default:
             return TAPPING_TERM;
     }
 }
 
+// cant remember why we did this. does it really make recognition faster? yeah it means that u can do tap term as 0 and still get a tap if you dont press anything else.
 bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LCAG_T(KC_ESC):
-        case LT(1, KC_SPC):
+        case LT(2, KC_SPC):
+        case OSL(2):
             return true;
         default:
             return false;
@@ -166,7 +170,9 @@ void lsft_finished(tap_dance_state_t *state, void *user_data) {
         case TD_DOUBLE_TAP:
             // Double tap: send shift twice
             // we closed the initial shift in lsft_each and now we just send a second
-            tap_code(KC_LSFT);
+            // tap_code(KC_LSFT);
+            // ok actually we're gonna hardcode map this to cmd+shift+space for the ghostty quick terminal
+            tap_code16(LSFT(LGUI(KC_SPC)));
             break;
         default:
             // insurance. just reset everything if tapped more than twice
