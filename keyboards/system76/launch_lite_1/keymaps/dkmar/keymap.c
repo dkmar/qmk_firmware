c5ace7d6542dd70ac04dbfa1e90fd685fe2b5d9f
dkmar <dkmar@protonmail.com>
2025-10-29 03:06:14 -0700

    fixed

--- keyboards/system76/launch_lite_1/keymaps/dkmar/keymap.c
+++ keyboards/system76/launch_lite_1/keymaps/dkmar/keymap.c
@@ -76,7 +76,7 @@ ________________________________________________________________________________
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_PGUP,
           LCAG_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,       KC_PGDN,
           TD(TD_LSFT),     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP, KC_END,
-          OSM(KC_LCTL), KC_LCTL,   KC_LALT, KC_LGUI,    KC_SPC,  OSL(2),    OSM(MOD_RGUI),    KC_RALT,   KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
+          OSM(MOD_LCTL), KC_LCTL,   KC_LALT, KC_LGUI,    KC_SPC,  OSL(2),    OSM(MOD_RGUI),    KC_RALT,   KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
 
